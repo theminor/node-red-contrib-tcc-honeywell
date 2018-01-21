339e9e57761c63dc16646523f71df2f2c5f00f9c
John <theminor@users.noreply.github.com>
2018-01-20 20:20:10 -0500

    convert to node ref instead of connection

--- node-tcc-honeywell.js
+++ node-tcc-honeywell.js
@@ -5,42 +5,39 @@ var hdrs = require('./requestHeaders');
 const ConnectSuccess = 200;
 const PostSuccess = 302;
 
-// expects an object that tracks status and stores connection info, incuding at least the following:
-// {usrName: "user", passwd: "pass", deviceID: 123, jar: cookieJar}
-// and a callback function
-var tccLogin = function(connection, callback) {
-	request(hdrs.defaults(connection), function(getErr1, getResponse1) {	// GET 1
+var tccLogin = function(node, callback) {
+	request(hdrs.defaults(node), function(getErr1, getResponse1) {	// GET 1
 		if (getErr1 || getResponse1.statusCode != ConnectSuccess) {			// Status Code 200 = OK
-			console.log('Login to TCC Failed (error at first GET)', getErr1);
-			connection.connected = false;
-			connection.statusCode = getResponse1.statusCode;
-			return connection;
+			node.debug('Login to TCC Failed (error at first GET)', getErr1);
+			node.connected = false;
+			node.statusCode = getResponse1.statusCode;
+			return node;
 		} else {
-			console.log('Successful first GET Connection, Status Code ' + getResponse1.statusCode);
-			connection.statusCode = getResponse1.statusCode;
-			request(hdrs.postDefaults(connection), function (postErr, postResponse) {		// POST
+			node.debug('Successful first GET Connection, Status Code ' + getResponse1.statusCode);
+			node.statusCode = getResponse1.statusCode;
+			request(hdrs.postDefaults(node), function (postErr, postResponse) {		// POST
 				if (postErr || postResponse.statusCode != PostSuccess) {		// Status Code 302 is successful post (?)
-					console.log('Login to TCC Failed (POST error)', postErr);
-					if (postResponse) console.log(postResponse.statusCode);
-					connection.connected = false;
-					connection.statusCode = 'Post Error: ' + postErr.statusCode + '. Post Response: ' + (postResponse) ? postResponse.statusCode : '';
-					return connection;
+					node.debug('Login to TCC Failed (POST error)', postErr);
+					if (postResponse) node.debug(postResponse.statusCode);
+					node.connected = false;
+					node.statusCode = 'Post Error: ' + postErr.statusCode + '. Post Response: ' + (postResponse) ? postResponse.statusCode : '';
+					return node;
 				} else {
-					console.log('Successful POST, Status Code ' + postResponse.statusCode);
-					connection.statusCode = postResponse.statusCode
-					request(hdrs.defaults(connection), function(getErr2, getResponse2) {		// GET 2
+					node.debug('Successful POST, Status Code ' + postResponse.statusCode);
+					node.statusCode = postResponse.statusCode
+					request(hdrs.defaults(node), function(getErr2, getResponse2) {		// GET 2
 						if (getErr2 || getResponse2.statusCode != ConnectSuccess) {
-							console.log('Login to TCC Failed (error at second GET)', getErr2);
-							if (getResponse2) console.log(getResponse2.statusCode);
-							connection.connected = false;
-							connection.statusCode = 'Get Error: ' + getErr2 + '. Get Response: ' + (getResponse2) ? getResponse2.statusCode : '';
-							return connection;
+							node.debug('Login to TCC Failed (error at second GET)', getErr2);
+							if (getResponse2) node.debug(getResponse2.statusCode);
+							node.connected = false;
+							node.statusCode = 'Get Error: ' + getErr2 + '. Get Response: ' + (getResponse2) ? getResponse2.statusCode : '';
+							return node;
 						} else {
-							console.log('Successful second GET Connection, Status Code ' + getResponse2.statusCode);	// statusCode should be ConnectSuccess (200)
-							connection.connected = true;
-							connection.statusCode = getResponse2.statusCode;
-							callback(connection);
-							return connection;
+							node.debug('Successful second GET Connection, Status Code ' + getResponse2.statusCode);	// statusCode should be ConnectSuccess (200)
+							node.connected = true;
+							node.statusCode = getResponse2.statusCode;
+							callback(node);
+							return node;
 						}
 					});
 				}
@@ -49,29 +46,29 @@ var tccLogin = function(connection, callback) {
 	});
 };
 
-var tccStatus = function(connection, callback) {
-	request(hdrs.defaults(connection), function(statusErr, statusResponse) {
+var tccStatus = function(node, callback) {
+	request(hdrs.defaults(node), function(statusErr, statusResponse) {
 		if (statusErr || statusResponse.statusCode != ConnectSuccess || statusResponse.statusMessage != "OK") {
-			console.log('Login to TCC Failed (error at tccStatus() GET)', statusErr);
-			if (statusResponse) console.log(statusResponse.statusCode);
-			connection.connected = false;
-			connection.statusCode = statusResponse.statusCode;
-			return connection;
+			node.debug('Login to TCC Failed (error at tccStatus() GET)', statusErr);
+			if (statusResponse) node.debug(statusResponse.statusCode);
+			node.connected = false;
+			node.statusCode = statusResponse.statusCode;
+			return node;
 		} else {
-			console.log('Successful GET in tccStatus(), Status Code ' + statusResponse.statusCode);
-			console.log('JSON Response Body:\n');
-			console.log(statusResponse.body);
-			connection.connected = true;						// true whether or not the response can be parsed into an object
-			connection.statusCode = statusResponse.statusCode;
+			node.debug('Successful GET in tccStatus(), Status Code ' + statusResponse.statusCode);
+			node.debug('JSON Response Body:\n');
+			node.debug(statusResponse.body);
+			node.connected = true;						// true whether or not the response can be parsed into an object
+			node.statusCode = statusResponse.statusCode;
 			try {
-				connection.statusData = JSON.parse(statusResponse.body);
+				node.statusData = JSON.parse(statusResponse.body);
 			} catch(err) {
-				console.log('Error parsing JSON Response Body', err);
-				connection.statusCode = statusResponse.statusCode + ' - with error parsing JSON Response Body: ' + err;
-				connection.statusData = statusResponse.body;	// return the text of the response only. Should we return null instead, since it can't be parsed into an object?
+				node.debug('Error parsing JSON Response Body', err);
+				node.statusCode = statusResponse.statusCode + ' - with error parsing JSON Response Body: ' + err;
+				node.statusData = statusResponse.body;	// return the text of the response only. Should we return null instead, since it can't be parsed into an object?
 			}
-			callback(connection);
-			return connection;
+			callback(node);
+			return node;
 		}
 	});
 };
@@ -80,19 +77,20 @@ module.exports = function(RED) {
 	var reg = function(config) {
 		RED.nodes.createNode(this, config);
 		var node = this;
-		var connection = {usrName: this.credentials.username, passwd: this.credentials.password, deviceID: this.credentials.deviceID, jar: request.jar(), connected: false};		
+		node.jar = request.jar();
+		node.connected: false
 		node.on('input', function(msg) {
-			var sendMsg = function(connection) {
-				msg.payload = connection.statusData;
+			var sendMsg = function(node) {
+				msg.payload = node.statusData;
 				msg.title = 'Honeywell TCC Data';					// see https://github.com/node-red/node-red/wiki/Node-msg-Conventions
 				msg.description = 'JSON data from Honeywell TCC';
 				node.send(msg);
 			};
-			if (connection.connected) tccStatus(connection, sendMsg);
-			else tccLogin(connection, function(connection) {
-				if (connection.connected) tccStatus(connection, sendMsg);
+			if (node.connected) tccStatus(node, sendMsg);
+			else tccLogin(node, function(node) {
+				if (node.connected) tccStatus(node, sendMsg);
 			});
 		});
 	};
-	RED.nodes.registerType('tcc-honeywell', reg, { credentials: { username: {type:"text"}, password: {type:"password"},	deviceID: {type: "text"} } });
+	RED.nodes.registerType('tcc-honeywell', reg, { credentials: { username: {type: "text"}, password: {type: "password"},	deviceID: {type: "text"} } });
 };
