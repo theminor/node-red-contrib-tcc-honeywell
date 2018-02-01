b61182a62f9b705e20436cbce39cd58362fbacbf
John <theminor@users.noreply.github.com>
2018-02-01 18:22:42 -0500

    Simplifying. Not working - currently broken - still working on it!

--- node-tcc-honeywell.js
+++ node-tcc-honeywell.js
@@ -4,72 +4,58 @@ var hdrs = require('./requestHeaders');
 // status codes that apparently get returned from successful web requests
 const ConnectSuccess = 200;
 const PostSuccess = 302;
+const SuccessStatusMsg = 'OK';
 
-var tccLogin = function(node, callback) {
-	request(hdrs.defaults(node), function(getErr1, getResponse1) {	// GET 1
-		if (getErr1 || getResponse1.statusCode != ConnectSuccess) {			// Status Code 200 = OK
-			node.statusTxt = 'Login to TCC Failed (error at first GET): ' + getErr1;
+var tccRequest = function(node, headers, debugIdentifier, successStatusCode, callback) {
+	request(headers, function(err, response) {
+		node.statusTxt = debugIdentifier + ': ';
+		if (response) {
+			node.statusTxt += 'Response: ' + JSON.stringify(response);
+			if (response.statusCode) node.statusCode = response.statusCode;
+			if (response.body) {
+				try {
+					node.statusData = JSON.parse(response.body);
+				} catch(pErr) {
+					node.statusTxt = ' -- with error parsing JSON Response Body: ' + pErr;
+					node.statusData = response.body;	// if body can't be parsed as JSON, retun the body as-is
+				}
+			}
+		}
+		if (err || response.statusCode != successStatusCode || response.statusMessage ? response.statusMessage != SuccessStatusMsg : false) {	// possible issue: response.statusMessage may not always be "OK"? Need to verify
 			node.connected = false;
-			node.statusCode = getResponse1.statusCode;
+			node.statusTxt += 'Error: ' + err;
 			return node;
 		} else {
-			node.statusTxt = 'Successful first GET Connection - Status Code: ' + getResponse1.statusCode; 
-			node.statusCode = getResponse1.statusCode;
-			request(hdrs.postDefaults(node), function (postErr, postResponse) {		// POST
-				if (postErr || postResponse.statusCode != PostSuccess) {		// Status Code 302 is successful post (?)
-					node.statusTxt = 'Login to TCC Failed (POST error): ' + postErr;
-					if (postResponse) node.statusTxt += ' -- Post response: ' + postResponse.statusCode;
-					node.connected = false;
-					node.statusCode = 'Post Error: ' + postErr.statusCode + '. Post Response: ' + (postResponse) ? postResponse.statusCode : '';
-					return node;
-				} else {
-					node.statusTxt = 'Successful POST - Status Code: ' + postResponse.statusCode;
-					node.statusCode = postResponse.statusCode
-					request(hdrs.defaults(node), function(getErr2, getResponse2) {		// GET 2
-						if (getErr2 || getResponse2.statusCode != ConnectSuccess) {
-							node.statusTxt = 'Login to TCC Failed (error at second GET): ' + getErr2;
-							if (getResponse2) node.statusTxt += ' -- Status Code: ' + getResponse2.statusCode;
-							node.connected = false;
-							node.statusCode = 'Get Error: ' + getErr2 + '. Get Response: ' + (getResponse2) ? getResponse2.statusCode : '';
-							return node;
-						} else {
-							node.statusTxt = 'Successful second GET Connection - Status Code: ' + getResponse2.statusCode;	// statusCode should be ConnectSuccess (200)
-							node.connected = true;
-							node.statusCode = getResponse2.statusCode;
-							callback(node);
-							return node;
-						}
-					});
-				}
-			});
+			node.statusTxt += ' -- success';
+			callback(node)
 		}
-	});
+	};
+};
+
+var tccLogin = function(node, callback) {
+	tccRequest(node, hdrs.defaults(node), 'TCC Login first GET', ConnectSuccess, function(node) {
+		tccRequest(node, hdrs.postDefaults(node), 'TCC Login POST', PostSuccess, function(node) {
+			tccRequest(node, hdrs.defaults(node), 'TCC Login second GET', ConnectSuccess, function(node) {
+				node.connected = true;
+				callback(node);
+			};
+		};
+	};
 };
 
 var tccStatus = function(node, callback) {
-	request(hdrs.getStatusDefaults(node), function(statusErr, statusResponse) {
-		if (statusErr || statusResponse.statusCode != ConnectSuccess || statusResponse.statusMessage != "OK") {
-			node.statusTxt = 'Login to TCC Failed (error at tccStatus() GET): ' + statusErr;
-			if (statusResponse) node.statusTxt += ' -- Status Code: ' + statusResponse.statusCode;
-			node.connected = false;
-			node.statusCode = statusResponse.statusCode;
-			return node;
-		} else {
-			node.statusTxt = 'Successful GET in tccStatus() - Status Code: ' + statusResponse.statusCode + ' -- JSON Response Body:\n' + statusResponse.body;
-			node.connected = true;						// true whether or not the response can be parsed into an object
-			node.statusCode = statusResponse.statusCode;
-			try {
-				node.statusData = JSON.parse(statusResponse.body);
-			} catch(err) {
-				node.statusTxt = 'Error parsing JSON Response Body: ' + err;
-				node.statusCode = statusResponse.statusCode + ' - with error parsing JSON Response Body: ' + err;
-				node.statusData = statusResponse.body;	// return the text of the response only. Should we return null instead, since it can't be parsed into an object?
-			}
-			callback(node);
-			return node;
-		}
-	});
-};
+	tccRequest(node, getStatusDefaults(node), 'TCC Status GET', ConnectSuccess, function(node) {
+		node.connected = true;						// true whether or not the response can be parsed into an object
+		callback(node);
+	};
+}
+
+// *** HERE - TO DO:
+var tccChangeSetting = function(settingsObj, node, callback) {
+}
+// *** old version below
+
+
 
 // structure of input (msg.payload):
 // if an object or JSON is recieved, the passed settings will be sent to the thermostat. For example, to set the cool setpoint until a time:
@@ -107,13 +93,14 @@ module.exports = function(RED) {
 		node.connected = false;
 		node.on('input', function(msg) {			
 			if (typeof msg.payload === 'string' || msg.payload instanceof String) {
+				node.warn('string recvd: ' + msg.payload);
 				try {
 					msg.payload = JSON.parse(msg.payload);
 				} catch(e) {
 					msg.payload = 'status';									// a recieved payload of 'status' (or anything that can't be parsed) will just check the status
 				}
 			} else {
-																			// it's something else - we assume it is an object, if not a string. So do nothing (?)
+																			// *** TO DO - it's something else - we assume it is an object, if not a string. So do nothing (?)
 			}
 			var sendMsg = function(node) {
 				msg.payload = node.statusData;
@@ -121,19 +108,23 @@ module.exports = function(RED) {
 				msg.description = 'JSON data from Honeywell TCC';
 				node.send(msg);
 			};
+			var callTherm()
+			
+			
+			
 			if (node.connected) {
-				if (msg.payload == 'status') {								// request for a simple status update
+				if (msg.payload === 'status') {								// request for a simple status update
 					tccStatus(node, sendMsg);
 				} else {
-					tccChangeSetting(setting, val, node, sendMsg);			// **** TO DO - change from a setting/val model to an object passed!
+					tccChangeSetting(msg.payload, node, sendMsg);			// **** TO DO - change from a setting/val model to an object passed!
 				}															// some other node data recieved - assume it is object with data to modify
 			} else {														// not connected, so try to connect first								
 				tccLogin(node, function(node) {
 					if (node.connected) {
-						if (msg.payload == 'status') {						// request for a simple status update
+						if (msg.payload === 'status') {						// request for a simple status update
 							tccStatus(node, sendMsg);
 						} else {											// some other node data recieved - assume it is object with data to modify
-							tccChangeSetting(setting, val, node, sendMsg);	// **** TO DO - change from a setting/val model to an object passed!
+							tccChangeSetting(msg.payload, node, sendMsg);
 						}
 					}
 				});
